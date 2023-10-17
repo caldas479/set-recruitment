b3f6d440dfafb4e11eca02e554a0f9cb1225d656
ist199125 <tiago.rodrigues.caldas@tecnico.ulisboa.pt>
2023-10-17 17:50:14 +0100

    add todo interface and method

--- lib/backend/services/todo.ts
+++ lib/backend/services/todo.ts
@@ -1,11 +1,47 @@
+interface ITodo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
 class Todo {
-  todos: Array<string>;
+  todos: Array<ITodo>;
 
   constructor() {
     this.todos = new Array();
   }
 
-  /* Add your todo methods here */
+  idExists(id: number): boolean {
+    return this.todos.some((todo) => todo.id === id);
+  }
+
+  getToDos() {
+    return this.todos;
+  }
+
+  getToDoById(id: number): ITodo {
+    return this.todos[id];
+  }
+
+  addToDo(text: string): ITodo {
+    const newTodo: ITodo = {
+      id: this.todos.length + 1,
+      text,
+      completed: false,
+    };
+    this.todos.push(newTodo);
+    return newTodo;
+  }
+
+  updateToDo(id: number, newText: string): ITodo{
+    this.todos[id].text = newText;
+    return this.todos[id];
+  }
+
+  deleteToDo(id: number): ITodo {
+    const deletedTodo = this.todos.splice(id, 1)[0];
+    return deletedTodo;
+  }
 }
 
 // Todo Service lifecycle management
@@ -13,3 +49,4 @@ const todo = global.todo || new Todo();
 if (process.env.NODE_ENV !== "production") global.todo = todo;
 
 export { Todo, todo as TodoService };
+export type { ITodo };
